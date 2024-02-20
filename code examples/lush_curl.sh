ee32f0c81ce7b11b5ed1f8538a433c41673fd358
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:55:41 +0200

    Adding code examples/lush_curl.sh

---
+++ code examples/lush_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.lush.com/us/en_us/c/hair-care?gad_source=1&gclid=cjwkcaiaungubhakeiwagid4am0kiglhmkfpbgcpf2dccwnx9sjfbaahgubt2cpwy2b8cq2hjv5eohochhgqavd_bwe"}'
